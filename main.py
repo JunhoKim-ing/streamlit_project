033fb71a134728b1e869808f56ae40e8856c2b00
leedongjin <114423304+leedongjin001224@users.noreply.github.com>
2024-07-06 00:29:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import streamlit as st
 import pandas as pd
 import datetime
 from streamlit_gsheets import GSheetsConnection
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+with open('./.streamlit/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+    )
 
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 SEX_TYPES = ["Male", "Female", "Others"]
+
 
 def read_data():
     data = conn.read(worksheet="sheet1", usecols=list(range(6)), ttl=3600)
@@ -15,25 +30,31 @@ def read_data():
 def write_data(data):
     conn.write(data)
 
-
 def main():
-    data = read_data()
+    if st.session_state["authentication_status"]:
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
+        try:
+            if authenticator.update_user_details(st.session_state["username"]):
+                print(st.session_state['username'])
+                with open('./.streamlit/config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('Entries updated successfully')
+        except Exception as e:
+            st.error(e)
+        if config["credentials"]["usernames"][st.session_state["name"]]["email"] in config["pre-authorized"]["emails"]:
+            admin()
 
-    st.title("Student Database Management")
-    st.header("Current Student Data")
-    st.dataframe(data)
+        authenticator.logout()
 
-    option = st.sidebar.selectbox(
-        'Select an operation',
-        ('View Data', 'Add Data', 'Remove Data', 'Filter/Search')
-    )
 
-    if option == 'Add Data':
-        add_data(data)
-    elif option == 'Remove Data':
-        remove_data(data)
-    elif option == 'Filter/Search':
-        filter_search_data(data)
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+        help()
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+        help()
+
 
 def add_data(data):
     st.header("Add New Student")
@@ -74,10 +95,10 @@ def remove_data(data):
     target_student_id = st.text_input("Enter Student ID to remove:")
 
     if st.button("Remove Student"):
-        data_filtered = data[data["Student ID"] == target_student_id]
+        data_filtered = data[data["Student ID"] == int(target_student_id)]
         if not data_filtered.empty:
-            data = data[data["Student ID"] != target_student_id]
-            write_data(data)
+            updated_df = data[data["Student ID"] != int(target_student_id)]
+            conn.update(worksheet="sheet1", data=updated_df)
             st.success(f"Student with ID {target_student_id} removed successfully!")
         else:
             st.warning(f"No student found with ID {target_student_id}.")
@@ -91,6 +112,86 @@ def filter_search_data(data):
         filtered_data = data[data.apply(lambda row: search_term.lower() in row.astype(str).str.lower().values, axis=1)]
         st.dataframe(filtered_data)
 
+def help():
+    option = st.selectbox('Help?', ['--Select--', 'Forgot username', 'Forgot password', 'New register'])
+
+    if option == 'Forgot password':
+        try:
+            username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+            if username_of_forgotten_password:
+                #############
+                with open('./.streamlit/config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('New password to be sent to your email')
+            elif username_of_forgotten_password == False:
+                st.error('Username not found')
+        except Exception as e:
+            st.error(e)
+
+    elif option == 'Forgot username':
+        try:
+            username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+            if username_of_forgotten_username:
+                ##############
+                st.success('Username to be sent to your email')
+            elif username_of_forgotten_username == False:
+                st.error('Email not found')
+        except Exception as e:
+            st.error(e)
+
+    elif option == 'New register':
+        try:
+            email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+            if email_of_registered_user:
+                with open('./.streamlit/config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('User registered successfully')
+        except Exception as e:
+            st.error(e)
+
+
+def admin():
+    data = read_data()
+
+    with st.container():
+        st.title("Student Database Management")
+        st.header("Current Student Data")
+        st.dataframe(data)
+        #if data_changed:
+        #    st.dataframe(data)
+
+
+    option = st.sidebar.selectbox(
+        'Select an operation',
+        ('View Data', 'Add Data', 'Remove Data', 'Filter/Search')
+    )
+
+    if option == 'Add Data':
+        add_data(data)
+    elif option == 'Remove Data':
+        remove_data(data)
+    elif option == 'Filter/Search':
+        filter_search_data(data)
+
+def viewer():
+    data = read_data()
+
+    with st.container():
+        st.title("Student Database Management")
+        st.header("Current Student Data")
+        st.dataframe(data)
+        #if data_changed:
+        #    st.dataframe(data)
+
+
+    option = st.sidebar.selectbox(
+        'Select an operation',
+        ('View Data', 'Filter/Search')
+    )
+    
+    if option == 'Filter/Search':
+        filter_search_data(data)
 
 if __name__ == "__main__":
+    authenticator.login()
     main()
