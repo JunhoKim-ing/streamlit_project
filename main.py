118225fa8917fbb6152e96a3c411c87435fbcd31
Leedongjin <leedongjin001224@gmail.com>
2024-08-02 01:03:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ users = users.set_index(users.keys()[0])
 config = {
     'credentials' : users.to_dict('index'),
     'pre-authorized' : conn_gsheet.read(worksheet="pre-authorized", usecols=list(range(1)), ttl=5)['emails'].to_list(),
-    'cookie' : conn_gsheet.read(worksheet="cookie", usecols=list(range(1)), ttl=5).to_dict('records')[0]
+    'cookie' : conn_gsheet.read(worksheet="cookie", usecols=list(range(3)), ttl=5).to_dict('records')[0]
 }
 
 authenticator = stauth.Authenticate(
