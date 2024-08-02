02708a42a2caf47edf4a179ebd06d1aef5127516
Leedongjin <leedongjin001224@gmail.com>
2024-08-02 16:29:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ def write_data(data):
     conn_gsheet.write(data)
 
 def main():
+    st.error(conn_gsheet.keys())
     if st.session_state["authentication_status"]:
         st.write(f'Welcome *{st.session_state["name"]}*')
         try:
